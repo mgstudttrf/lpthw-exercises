7ae41fb653452a2a5e061aace5e3a0ef04a10d67
mgstudttrf <fesss678fesss@gmail.com>
2018-09-27 00:54:15 +0300

    third

---
+++ ex2.py
@@ -0,0 +1,6 @@
+# a comment,This is so you can read your program later
+#Anything after # is ignored by python
+print("i could have code like this")#th3 redt is ignored
+#u can also use commentsbto disable things like
+#print("5his wont run")
+print("this will")
